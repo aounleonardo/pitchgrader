1b5a8e8a24f75664834ed323c5ed5d956426e8d0
Leonardo Aoun <aounleo@Leonardos-MacBook-Pro.local>
2018-05-01 01:26:15 +0200

    basic mapbox

--- pitchgrader/src/App.js
+++ pitchgrader/src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
 
 
+const Mapbox = ReactMapboxGl({
+    accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+    minZoom: 10,
+    maxZoom: 20
+});
 
 class App extends Component {
     render() {
         return (
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                    <li>{process.env.REACT_APP_MAPBOX_TOKEN}</li>
-                </ul>
+            <div>
+                <h1>Hello {this.props.name}</h1>
+                <Mapbox style="mapbox://styles/mapbox/streets-v9"
+                     center={[6.5668, 46.5191]}
+                        zoom={[15]}
+                     containerStyle={{
+                         height: "100vh",
+                         width: "100vw"
+                     }}>
+                    <Layer type="symbol" id="marker" layout={{"icon-image": "marker-15"}}>
+                        <Feature coordinates={[6.5668, 46.5191]}/>
+                    </Layer>
+                </Mapbox>
             </div>
         );
     }
